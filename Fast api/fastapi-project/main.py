702ed881806f4afb3835965abbe6e6490a787cbf
UMER YASEEN <umery1730@gmail.com>
2026-07-26 01:21:28 +0500

    create about function

--- Fast api/fastapi-project/main.py
+++ Fast api/fastapi-project/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "My first API is working"}
+
+@app.get("/about")
+def about():
+    return {"project": "This model is very risky", 
+            "version":"1.0"
+            }
