7ad1947873476cacee4d1b3041645edd5801e840
UMER YASEEN <umery1730@gmail.com>
2026-07-29 21:47:21 +0500

    nested models

--- Fast api/Day-2/pydan.py
+++ Fast api/Day-2/pydan.py
@@ -4,12 +4,17 @@ from typing import Optional
 
 app = FastAPI()
 
+class Address(BaseModel):
+    city: str
+    country: str
+
 class Student(BaseModel):
     name: str
     age: int
     reg: str
     department: str = "Computer Science"
     semester: Optional[int] = None
+    address: Address
 
 @app.post("/student")
 def create_student(student: Student):
