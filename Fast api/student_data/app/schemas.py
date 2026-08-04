9ebf02daba912288394599d11e073fe1513cacbd
UMER YASEEN <umery1730@gmail.com>
2026-08-04 20:10:47 +0500

    update the student respnse

--- Fast api/student_data/app/schemas.py
+++ Fast api/student_data/app/schemas.py
@@ -3,3 +3,15 @@ from pydantic import BaseModel
 class StudentCreate(BaseModel):
     name: str
     department: str
+    age: int
+    email: str
+
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    department: str
+    age: int
+    email: str
+
+    class Config:
+        from_attributes = True
