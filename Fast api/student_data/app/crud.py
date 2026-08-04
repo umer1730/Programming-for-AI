4f83f94843cb7966e783ff0d1a4fec087b1c8039
UMER YASEEN <umery1730@gmail.com>
2026-08-04 20:08:13 +0500

    add two columns

--- Fast api/student_data/app/crud.py
+++ Fast api/student_data/app/crud.py
@@ -6,7 +6,9 @@ from app.schemas import StudentCreate
 def create_student(db: Session, student: StudentCreate):
     new_student = Student(
         name=student.name,
-        department=student.department
+        department=student.department,
+        age=student.age,
+        email=student.email
     )
 
     db.add(new_student)
@@ -18,3 +20,31 @@ def create_student(db: Session, student: StudentCreate):
 
 def get_students(db: Session):
     return db.query(Student).all() #Using the database connection (db), query the Student table and return all records
+
+def get_student(db: Session, student_id: int):
+    return db.query(Student).filter(
+        Student.id == student_id
+    ).first()
+
+def update_student(db: Session,student_id: int,student: StudentCreate):
+    db_student = get_student(db,student_id)
+    if db_student is None:
+        return None
+
+    db_student.name = student.name
+    db_student.department = student.department
+
+    db.commit()
+    db.refresh(db_student)
+
+    return db_student
+
+def delete_student(db: Session,student_id: int):
+    student = get_student(db,student_id)
+    if student is None:
+        return None
+
+    db.delete(student)
+    db.commit()
+
+    return student 
