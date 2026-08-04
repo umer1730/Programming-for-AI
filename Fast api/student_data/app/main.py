f2866d7ff248c720d7e229f1481ac5e5815d7b87
UMER YASEEN <umery1730@gmail.com>
2026-08-04 20:09:15 +0500

    update the get add and delete function

--- Fast api/student_data/app/main.py
+++ Fast api/student_data/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db, engine, Base
@@ -17,3 +17,42 @@ def create_student(student: StudentCreate, db: Session = Depends(get_db)):
 @app.get("/students")
 def get_students(db: Session = Depends(get_db)):
     return crud.get_students(db)
+
+@app.get("/students/{student_id}")
+def read_student(student_id: int,db: Session = Depends(get_db)):
+    student = crud.get_student(db, student_id)
+
+    if student is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return student
+
+
+@app.put("/students/{studnet_id}")
+def update(student_id: int,student: StudentCreate,db: Session = Depends(get_db)):
+
+    updated = crud.update_student(db,student_id,student)
+
+    if updated is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    return updated
+
+@app.delete("/students/{student_id}")
+def delete(student_id: int, db: Session = Depends(get_db)):
+    deleted = crud.delete_student(db,student_id)
+
+    if deleted is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return {
+        "message": "Student deleted successfully"
+    }
