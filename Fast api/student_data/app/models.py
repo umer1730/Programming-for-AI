8607244b2f17a08baf5bf058f3ac3af89187a86c
UMER YASEEN <umery1730@gmail.com>
2026-08-04 20:09:53 +0500

    add two columns in models

--- Fast api/student_data/app/models.py
+++ Fast api/student_data/app/models.py
@@ -2,8 +2,10 @@ from sqlalchemy import Column, Integer, String
 from app.database import Base
 
 class Student(Base):
-    __tablename__ = "Students"
+    __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     department = Column(String)
+    age = Column(Integer)
+    email = Column(String)
