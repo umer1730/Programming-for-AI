3a1f622a7fa6da9d241c0cd4ea77ff201cebf33b
UMER YASEEN <umery1730@gmail.com>
2026-08-13 20:12:34 +0500

    change prediction input to prediction request

--- Fast api/ML_pred_api/schemas.py
+++ Fast api/ML_pred_api/schemas.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 
-class PredictionInput(BaseModel):
+class PredictionRequest(BaseModel):
     input_value: float
+    model_name: str
 
 class PredictionResponse(BaseModel):
     id: int
     input_value: float
     prediction: float
+    model_name: str
 
     class Config:
         from_attributes = True 
